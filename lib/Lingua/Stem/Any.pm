fefb9d41ddc711b5d7557b5431883534163446e3
Nick Patch <patch@cpan.org>
2013-04-02 20:47:56 -0400

    update docs

--- lib/Lingua/Stem/Any.pm
+++ lib/Lingua/Stem/Any.pm
@@ -31,31 +31,31 @@ This document describes Lingua::Stem::Any v0.00_1.
     # create German stemmer explicitly using Lingua::Stem::Snowball
     $stemmer = Lingua::Stem::Any->new(language => 'de', source => 'snowball');
 
-    # get stem for a word
+    # get stem for word
     $stem = $stemmer->stem($word);
 
-    # get list of stems for a list of words
-    @stems = $stemmer->stem(@stems);
+    # get list of stems for list of words
+    @stems = $stemmer->stem(@words);
 
     # replace words in array reference with stems
-    $stemmer->stem(\@stems);
+    $stemmer->stem(\@words);
 
-	# get supported languages
+    # get supported languages
     @languages = $stemmer->languages;
 
-	# get current language
+    # get current language
     $language = $stemmer->language;
 
-	# change language to Dutch
+    # change language to Dutch
     $stemmer->language('nl');
 
-	# get supported sources for the current language
+    # get supported sources for the current language
     @sources = $stemmer->sources;
 
-	# get current source
+    # get current source
     $source = $stemmer->source;
 
-	# change source to Lingua::Stem::UniNE
+    # change source to Lingua::Stem::UniNE
     $stemmer->source('unine');
 
 =head1 DESCRIPTION
@@ -76,27 +76,36 @@ This document describes Lingua::Stem::Any v0.00_1.
 
 The first source listed for a language is the default.
 
-    Arabic      ar  Lingua::AR::Word
-    Bulgarian   bg  Lingua::Stem::UniNE
-    Czech       cs  Lingua::Stem::UniNE
-    Danish      da  Lingua::Stem::Snowball, Lingua::Stem::Snowball::Da
-    Dutch       nl  Lingua::Stem::Snowball
-    English     en  Lingua::Stem::Snowball, Lingua::Stem::En, SWISH::Stemmer
-    Finnish     fi  Lingua::Stem::Snowball
-    French      fr  Lingua::Stem::Snowball, Lingua::Stem::Fr
-    Galician    gl  Lingua::GL::Stemmer
-    German      de  Lingua::Stem::Snowball, Text::German
-    Hungarian   hu  Lingua::Stem::Snowball
-    Italian     it  Lingua::Stem::Snowball, Lingua::Stem::It
-    Latin       la  Lingua::LA::Stemmer
-    Norwegian   no  Lingua::Stem::Snowball, Lingua::Stem::Snowball::No
-    Persian     FA  Lingua::Stem::UniNE
-    Portuguese  pt  Lingua::Stem::Snowball, Lingua::PT::Stemmer
-    Romanian    ro  Lingua::Stem::Snowball
-    Russian     ru  Lingua::Stem::Snowball, Lingua::Stem::Ru
-    Spanish     es  Lingua::Stem::Snowball, Lingua::Stem::Es
-    Swedish     sv  Lingua::Stem::Snowball, Lingua::Stem::Snowball::Se
-    Turkish     tr  Lingua::Stem::Snowball
+    ┌────────────┬────┬──────────────────────────────────────────────────────────┐
+    │ Arabic     │ ar │ Lingua::AR::Word                                         │
+    │ Bulgarian  │ bg │ Lingua::Stem::UniNE                                      │
+    │ Czech      │ cs │ Lingua::Stem::UniNE                                      │
+    │ Danish     │ da │ Lingua::Stem::Snowball, Lingua::Stem::Snowball::Da       │
+    │ Dutch      │ nl │ Lingua::Stem::Snowball                                   │
+    │ English    │ en │ Lingua::Stem::Snowball, Lingua::Stem::En, SWISH::Stemmer │
+    │ Finnish    │ fi │ Lingua::Stem::Snowball                                   │
+    │ French     │ fr │ Lingua::Stem::Snowball, Lingua::Stem::Fr                 │
+    │ Galician   │ gl │ Lingua::GL::Stemmer                                      │
+    │ German     │ de │ Lingua::Stem::Snowball, Text::German                     │
+    │ Hungarian  │ hu │ Lingua::Stem::Snowball                                   │
+    │ Italian    │ it │ Lingua::Stem::Snowball, Lingua::Stem::It                 │
+    │ Latin      │ la │ Lingua::LA::Stemmer                                      │
+    │ Norwegian  │ no │ Lingua::Stem::Snowball, Lingua::Stem::Snowball::No       │
+    │ Persian    │ fa │ Lingua::Stem::UniNE                                      │
+    │ Portuguese │ pt │ Lingua::Stem::Snowball, Lingua::PT::Stemmer              │
+    │ Romanian   │ ro │ Lingua::Stem::Snowball                                   │
+    │ Russian    │ ru │ Lingua::Stem::Snowball, Lingua::Stem::Ru                 │
+    │ Spanish    │ es │ Lingua::Stem::Snowball, Lingua::Stem::Es                 │
+    │ Swedish    │ sv │ Lingua::Stem::Snowball, Lingua::Stem::Snowball::Se       │
+    │ Turkish    │ tr │ Lingua::Stem::Snowball                                   │
+    └────────────┴────┴──────────────────────────────────────────────────────────┘
+
+=head1 ACKNOWLEDGEMENTS
+
+This module is brought to you by L<Shutterstock|http://www.shutterstock.com/>
+(L<@ShutterTech|https://twitter.com/ShutterTech>).  Additional open source
+projects from Shutterstock can be found at
+L<code.shutterstock.com|http://code.shutterstock.com/>.
 
 =head1 AUTHOR
 
