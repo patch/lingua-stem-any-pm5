31318a208428787e60160081afb86256593a11bb
Nick Patch <patch@cpan.org>
2014-08-27 21:13:45 -0400

    prepare v0.04

--- lib/Lingua/Stem/Any.pm
+++ lib/Lingua/Stem/Any.pm
@@ -10,7 +10,7 @@ use Unicode::Normalize qw( NFC );
 use Moo;
 use namespace::clean;
 
-our $VERSION = '0.03_1';
+our $VERSION = '0.04';
 
 my %language_alias = (
     nb => 'no',
@@ -309,7 +309,7 @@ Lingua::Stem::Any - Unified interface to any stemmer on CPAN
 
 =head1 VERSION
 
-This document describes Lingua::Stem::Any v0.03_1.
+This document describes Lingua::Stem::Any v0.04.
 
 =head1 SYNOPSIS
 
